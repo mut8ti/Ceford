4efefbfec7ccad65949b2509139f8d2ae88bfb79
WolfeLeo2 <muneneboizkaruga@gmail.com>
2025-09-04 15:49:27 +0300

    removed caching system

---
+++ js/unregister-sw.js
@@ -0,0 +1,24 @@
+// Unregister any existing service workers
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(function(registrations) {
+    for(let registration of registrations) {
+      registration.unregister().then(function(boolean) {
+        console.log('Service Worker unregistered:', boolean);
+      });
+    }
+  });
+  
+  // Clear all caches
+  if ('caches' in window) {
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          console.log('Deleting cache:', cacheName);
+          return caches.delete(cacheName);
+        })
+      );
+    }).then(function() {
+      console.log('All caches cleared');
+    });
+  }
+}
